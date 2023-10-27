edc4b5deb95b69ae029164e986eaa4d8aa4b00ac
franklin-raph <igboekwulusifranklin@gmail.com>
2023-10-27 22:32:18 +0100

    update

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -6,15 +6,20 @@ const AboutUs = () => {
   return (
     <div>
         <Navbar />
-        <section className='flex items-center justify-between about'>
-            <div className='ml-[3rem] py-[8rem]'>
-              <p className='font-bold'>Our Story</p>
-                <h1 className='font-bold text-4xl mb-3'>The Creative Director Kraftea designz</h1>
+        <section className='py-[8rem]'>
+          <div className='text-center mb-[3rem]'>
+            <h2 className='font-bold text-2xl lg:text-4xl'>Our Story</h2>
+            <p>Lorem Ipsum</p>
+          </div>
+          <div className='flex items-center justify-between w-full about'>
+            <div className=''>
+                <h1 className='font-bold text-3xl mb-3'>The Creative Director Kraftea designz</h1>
                 <p>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos obcaecati voluptate dicta alias maxime beatae iusto vero incidunt hic cum laudantium, doloribus, praesentium, esse quae doloremque et dolorum exercitationem officia.
                 </p>
             </div>
             <img src={LandingImage} alt="" />
+          </div>
         </section>
     </div>
   )
