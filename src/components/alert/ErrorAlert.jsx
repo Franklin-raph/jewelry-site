30d477603bc6075a1aa1b2197ac29d355f7a994f
franklin-raph <igboekwulusifranklin@gmail.com>
2023-11-07 14:36:54 +0100

    update

--- src/components/alert/ErrorAlert.jsx
+++ src/components/alert/ErrorAlert.jsx
@@ -1,9 +1,20 @@
 import {useState} from 'react'
+import { useLocation } from "react-router-dom";
 
-const ErrorAlert = ({ error, setError }) => {
+const ErrorAlert = ({ error, setError, paymentUrl }) => {
     
     const [success, setSuccess] = useState("")
     const [loader, setLoader] = useState(false)
+    const location = useLocation();
+
+    function makepayment(){
+      window.location.href = paymentUrl
+    }
+
+    function cancelPayment(){
+      window.location.href = "/"
+    }
+
   return (
     <div>
       <div className="successModalBg">
@@ -25,6 +36,12 @@ const ErrorAlert = ({ error, setError }) => {
           ></i>
           <i class="ri-close-circle-line text-red-500 text-6xl"></i>
           <p style={{ color: "black" }}>{error}</p>
+          {location.pathname === "/confirm-payment" && 
+            <div className='flex items-center gap-5'>
+              <button onClick={() => makepayment()}>Ok</button>
+              <button onClick={() => cancelPayment()}>Cancel Payment</button>
+            </div>
+          }
         </div>
       </div>
     </div>
--- src/pages/confirm-payment/ConfirmPayment.jsx
+++ src/pages/confirm-payment/ConfirmPayment.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import ErrorAlert from '../../components/alert/ErrorAlert'
 
-const ConfirmPayment = () => {
+const ConfirmPayment = ({baseUrl}) => {
+
+    const sessionId = JSON.parse(localStorage.getItem("sessionId"))
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [paymentUrl, setPaymentUrl] = useState("")
+    const [success, setSuccess] = useState("")
+
+    useEffect(() => {
+        confirmPayment()
+    },[])
+
+    async function confirmPayment(){
+        setIsLoading(true)
+        const response = await fetch(`${baseUrl}/confirm-payment/?session_id=${sessionId}`)
+        const data = await response.json()
+        if(response) setIsLoading(false)
+        if(!response.ok){
+            setError(data.detail)
+            console.log(data.detail)
+            setPaymentUrl(data.payment_url)
+        }
+        console.log(response, data)
+    }
+
   return (
     <div>
         <Navbar />
         <div className='pt-[5rem] px-12 pb-[5rem] mb-[5rem]'>
-            <h1 className='mb-4 font-bold'>My Order Summary</h1>
+            {isLoading && 
+                <>
+                    <div className='loader-bg1'>
+                        <div className="loader-bg2">
+                            <i class="fa-solid fa-gear fa-spin text-2xl"></i>
+                        </div>
+                    </div>
+                    <h1 className='mb-4 font-bold text-center fixed' style={{ top:"40%", left:"50%", transform:"translate(-50%, -50%)" }}>Your payment is been confirmed, please wait</h1>
+                </>
+            }
+            {error &&
+                <ErrorAlert error={error} setError={setError} paymentUrl={paymentUrl}/>
+            }
         </div>
     </div>
   )
